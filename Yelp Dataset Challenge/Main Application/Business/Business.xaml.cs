3068cab6ad9049b85fe2d5cc1e4ad44bb86ae92d
David Fletcher <david.e.fletcher@email.wsu.edu>
2015-08-07 19:01:30 -0700

    added attributes
    added attributes, cleaned up usings.

--- Yelp Dataset Challenge/Main Application/Business/Business.xaml.cs
+++ Yelp Dataset Challenge/Main Application/Business/Business.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Yelp_Dataset_Challenge
 {
@@ -44,6 +33,7 @@ namespace Yelp_Dataset_Challenge
         /// Updated August 7th, 2015 - David Fletcher
         ///     - Simplified initializer to reduce sql queries and to help optimize
         ///     - Added hours display
+        ///     - Added tips display
         /// </summary>
         /// <param name="bID"></param>
         private void initializeLabels(string bID)
@@ -77,12 +67,27 @@ namespace Yelp_Dataset_Challenge
                 textBox.Text += elements[0] + " open : " + elements[1] + " close : " + elements[2] + "\n";
             }
 
+            // attributes
+            sqlQuery = "SELECT attribute_name, attribute_value FROM attributeTable WHERE business_id LIKE '" + bID + "' ORDER BY attribute_name ASC;";
+            list = con.sqlSelect(sqlQuery, true);
 
+            for(int i = 0; i < list.Count; i+=2)
+            {
+                if (i == 0)
+                {
+                    textBox.Text += "attributes\n";
+                    textBox.Text += "--------------------------------------------------------------------------\n";
+                }
+
+                elements = list[i].Split(';');
+                textBox.Text += elements[0] + " " + elements[1] + "\n";
+            }
+            textBox.Text += "\n";
+
+            // reviews
             sqlQuery = "SELECT * FROM reviewTable WHERE business_id LIKE '" + bID + "';";
             list.Clear();
             list = con.sqlSelect(sqlQuery, true);
-
-
 
             // cycle through reviews
             for (int i = 0; i < list.Count; i++)
@@ -101,9 +106,32 @@ namespace Yelp_Dataset_Challenge
 
                 textBox.Text += name[0] + " date : " + elements[5] + " stars : " + elements[3] + "\n";
                 textBox.Text += elements[4] + "\n";
-                textBox.Text += "votes funny : " + elements[6] + " useful : " + elements[7] + " cool : " + elements[8] + "\n";
+                textBox.Text += "votes funny : " + elements[6] + " useful : " + elements[7] + " cool : " + elements[8] + "\n\n";
             }
 
+            // tips
+            sqlQuery = "SELECT * FROM tipTable WHERE business_id LIKE '" + bID + "';";
+            list.Clear();
+            list = con.sqlSelect(sqlQuery, true);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == 0)
+                {
+                    textBox.Text += "tips\n";
+                    textBox.Text += "--------------------------------------------------------------------------\n";
+                }
+
+                elements = list[i].Split(';');
+
+                sqlQuery = "SELECT name FROM userTable WHERE user_id LIKE '" + elements[1] + "';";
+
+                List<string> name = new List<string>();
+                name = con.sqlSelect(sqlQuery, false);
+
+                textBox.Text += name[0] + " date : " + elements[3] + " likes : " + elements[4] + "\n";
+                textBox.Text += elements[2] + "\n\n";
+            }
         }
     }
 }
